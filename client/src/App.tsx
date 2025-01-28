2a4ddd9aaa38c7e41a20f80a7667983258fe8c28
Jagath K R <108512623+jagath411@users.noreply.github.com>
2025-01-28 21:25:43 +0530

    Folder Structues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,29 @@
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "@/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Navbar from "@/scenes/navbar";
+
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>Prediction page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Box, Typography, useTheme } from "@mui/material";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme();
+  return <Box display="flex" justifyContent="space-between"></Box>;
+};
+
+export default Navbar;
