c96788c330831cf4e6d5084c3a16c8726d1c35a4
Jagath K R <108512623+jagath411@users.noreply.github.com>
2025-01-29 21:37:53 +0530

    Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+// import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,8 +17,9 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>Prediction page</div>} />
             </Routes>
           </Box>
