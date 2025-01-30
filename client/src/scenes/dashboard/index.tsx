aa7cd8791c5cd1b888d6bed16997a6ed33a5fcf2
Jagath K R <108512623+jagath411@users.noreply.github.com>
2025-01-30 21:39:04 +0530

    grid chaanges

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import { useTheme } from "@emotion/react";
+import { Box, Palette } from "@mui/material";
 
 type Props = {};
 
+const gridtemplate = `
+"a b c"
+"a b c"
+"a b c"
+"a b f"
+"d e f"
+"d e f"
+"d f i"
+"g h i"
+"g h j"
+"g h j"
+`;
+
 const Dashboard = (props: Props) => {
-  return <div>Dashboard</div>;
+  const { palette } = useTheme();
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={{
+        gridTemplateColumns: "repeat(3,minmax(370x,1fr))",
+        gridTemplateRows: "repeat(10,minmax(60px,1fr))",
+        gridTemplateAreas: gridtemplate,
+      }}
+    >
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="b"></Box>
+      <Box bgcolor="#fff" gridArea="c"></Box>
+      <Box bgcolor="#fff" gridArea="d"></Box>
+      <Box bgcolor="#fff" gridArea="e"></Box>
+      <Box bgcolor="#fff" gridArea="f"></Box>
+      <Box bgcolor="#fff" gridArea="g"></Box>
+      <Box bgcolor="#fff" gridArea="h"></Box>
+      <Box bgcolor="#fff" gridArea="i"></Box>
+      <Box bgcolor="#fff" gridArea="j"></Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
